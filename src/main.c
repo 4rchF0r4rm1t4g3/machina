b257d4e7ebccf3068938c1f09a95d4589ee170da
Josef Aboulaban <josefaboulaban@Josefs-MacBook-Air.local>
2025-08-31 11:57:57 +0300

    adding the keyboard interrupt and b register

--- src/main.c
+++ src/main.c
@@ -13,8 +13,9 @@ int main(int argc, char *argv[])
 {
     int c;
     int by = 0;
+    int l = 0;
     unsigned char code[512];
-    memset(code, 0, 512);
+    int stw = 1000000;
 
     if (argc < 2)
     {
@@ -34,6 +35,64 @@ int main(int argc, char *argv[])
 
     char buffer[4096];
 
+    fgets(buffer, 4096, fptr);
+
+    char buff[4096];
+
+    int buflen = strlen(buffer);
+    if(buffer[buflen-1] == '\n')
+    {
+            buffer[buflen-1] = '\0';
+            buffer[buflen] = '\0';
+    }
+
+    int c1 = 0;
+
+    while(1)
+    {
+            if(buffer[c] != ' ')
+        {
+            break;
+        }
+
+        ++c;
+    }
+
+    int d = 0;
+
+    while(1)
+    {
+        if(buffer[d+c] == ' ')
+        {
+            break;
+        }
+
+        if(buffer[d+c] == '\0')
+        {
+            break;
+        }
+
+        ++d;
+    }
+
+    strncpy(buff, buffer+c, d);
+    if(buff[c+d+1] == EOF)
+    {
+    }
+
+    else
+    {
+        buff[c+d] = '\0';
+    }
+
+    if(strncmp(buff, ".sector", strlen(".sector")) == 0)
+    {
+        stw = 512;
+        memset(code, 0, stw);
+        code[510] = 0x55;
+        code[511] = 0xAA;
+    }
+    
 
     while(fgets(buffer, 4096, fptr))
     {
@@ -87,13 +146,19 @@ int main(int argc, char *argv[])
             buf[c+d] = '\0';
         }
 
-        int res = 0;
-
-        if((res = strncmp("i10", buffer+c, 3)) == 0)
+        if(strncmp("i10", buf, 3) == 0)
         {
             code[by] = 0xCD;
             by++;
             code[by] = 0x10;
+            by++;
+        }
+
+        if(strncmp("i16", buf, 3) == 0)
+        {
+            code[by] = 0xCD;
+            by++;
+            code[by] = 0x16;
             by++;
         }
         
@@ -348,7 +413,6 @@ int main(int argc, char *argv[])
             if(axbuf[0] == '\'')
             {
                 cbuf[0] = axbuf[1];
-                printf("%lu", sizeof(cbuf));
                 if(axbuf[2] != '\'')
                 {
                     printf("ERROR: he string does not end\n");
@@ -579,13 +643,124 @@ int main(int argc, char *argv[])
             nop();
         }
 
+        if(strncmp("bx", buf, 2) == 0)
+        {
+            int e = d+c;
+            int emod = e;
+
+            while(1)
+            {
+                if((buffer[emod] != ' ') && (buffer[emod] != '\0'))
+                {
+                    break;
+                }
+
+                if(buffer[emod] == '\0')
+                {
+                    printf("needed value for bx");
+                    exit(0);
+                }
+
+                emod++;
+            }
+
+            int nemod = emod;
+
+            while(1)
+            {
+                if((buffer[nemod] == '\0') || (buffer[nemod] == ' '))
+                {
+                    break;
+                }
+
+                nemod++;
+            }
+
+            char axbuf[4096];
+            char cbuf;
+            char axb1;
+            char axb[2];
+            int b2l;
+
+            code[by] = 0xBB;
+            by++;
+
+            strncpy(axbuf, buffer+emod, nemod);
+
+            if(axbuf[0] == '\'')
+            {
+                cbuf = axbuf[1];
+                if(axbuf[2] != '\'')
+                {
+                    printf("ERROR: he string does not end\n");
+                }
+
+                else
+                {
+                    code[by] = cbuf;
+                    by++;
+                    by++;
+                    goto bxend;
+                }
+            }
+            
+            if(strlen(axbuf) > 4)
+            {
+                printf("ERROR: the value tried to be loaded on ax is greater than 0xffff\n");
+                exit(1);
+            }
+
+            b2l = strlen(axbuf) - 2;
+
+            int ax1 = strlen(axbuf);
+
+            int b2 = 0;
+
+            if(ax1 <= 2)
+            {
+                strncpy(axb, axbuf, ax1);
+                axb1 = (int)strtol(axb, NULL, 16);
+            }
+
+            if(ax1 >= 2)
+            {
+                strncpy(axb, axbuf, 2);
+                axb1 = (int)strtol(axb, NULL, 16);
+            }
+
+            char axb0[2];
+            char axb2;
+
+            if(b2l > 0)
+            {
+                strncpy(axb0, axbuf+2, 2);
+                b2 = 1;
+                axb2 = (int)strtol(axb0, NULL, 16);
+            }
+
+            if(b2 == 1)
+            {
+                code[by] = axb2;
+                by++;
+                code[by] = axb1;
+                by++;
+            }
+
+            else
+            {
+                code[by] = axb1;
+                by++;
+                code[by] = 0x00;
+                by++;
+            }
+
+            bxend:
+            nop();
+        }
+
     }
-
-
-    code[510] = 0x55;
-    code[511] = 0xAA;
 
     FILE* out;
     out = fopen(argv[2], "wb");
-    fwrite(code, 1, 512, out);
+    fwrite(code, 1, stw, out);
 }
